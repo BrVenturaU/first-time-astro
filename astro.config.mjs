e6f1fe1bf63bd350e7dd788b05211e36920f6802
BrVenturaU <brandonventura16@gmail.com>
2024-01-06 20:21:48 -0600

    Adding islands.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://steady-granita-b6ab6b.netlify.app",
+  integrations: [preact()]
 });
